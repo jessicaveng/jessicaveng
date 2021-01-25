37f3b27bb84f8bc65c28f7362bb50c7bbc7c7955
Jessicaveng <jessicaveng@outlook.com>
2021-01-25 20:45:15 +1300

    initial Sass installation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,10 @@
-import '../styles/App.css';
-
+import '../styles/App.scss';
+import Homepage from './Homepage.jsx';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          HELLO
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Homepage/>
       </header>
     </div>
   );
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+   <div>
+       <p>HELLO WORLD</p>
+   </div>
+  );
+}
+
+export default Home;
