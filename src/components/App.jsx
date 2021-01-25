b2598f19842f8dc70df99c48dd80c046fc92583b
Jessicaveng <jessicaveng@outlook.com>
2021-01-25 16:29:47 +1300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import './styles/App.css';
+import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO
         </p>
         <a
           className="App-link"
